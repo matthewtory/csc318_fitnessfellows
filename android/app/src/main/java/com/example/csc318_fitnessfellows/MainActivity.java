a804842a1a2bedc6b5b9a5900e999f79ea826fc0
Matthew Tory <matthew.tory@mail.utoronto.ca>
2019-03-06 18:47:33 -0500

    Initial commit of flutter project

---
+++ android/app/src/main/java/com/example/csc318_fitnessfellows/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.csc318_fitnessfellows;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
